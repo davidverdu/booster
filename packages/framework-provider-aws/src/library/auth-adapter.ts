33bb9e64e1840205c6921b6c84370aaf9da7b670
alvaroloes <alvaro@booster.cloud>
2020-07-31 17:20:42 +0100

    AWS implement userEnvelopeFromAuthToken

--- packages/framework-provider-aws/src/library/auth-adapter.ts
+++ packages/framework-provider-aws/src/library/auth-adapter.ts
@@ -7,9 +7,15 @@ export function rawSignUpDataToUserEnvelope(rawMessage: CognitoUserPoolEvent): U
   return UserEnvelopeBuilder.fromAttributeMap(rawMessage.request.userAttributes)
 }
 
-export function userEnvelopeFromAuthToken(
+export async function userEnvelopeFromAuthToken(
   userPool: CognitoIdentityServiceProvider,
-  token: string
-): UserEnvelope | undefined {
-  return undefined
+  token: string | undefined
+): Promise<UserEnvelope | undefined> {
+  if (!token) {
+    return undefined
+  }
+  const accessToken = token.replace('Bearer ', '')
+  const currentUserData = await userPool.getUser({ AccessToken: accessToken }).promise()
+  // TODO: Check the errors for when the token is expired, etc. and throw with corresponding types and then use HTTP status codes
+  return UserEnvelopeBuilder.fromAttributeList(currentUserData.UserAttributes)
 }
